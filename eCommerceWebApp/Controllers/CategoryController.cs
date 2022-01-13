cbe4ce2bfef75a9a5ef4e9faa64eefe0c70b38ba
luiz guilherme luize pereira <superguili@gmail.com>
2022-01-13 12:50:00 +0000

    Added Validation to server and client side

--- eCommerceWebApp/Controllers/CategoryController.cs
+++ eCommerceWebApp/Controllers/CategoryController.cs
@@ -19,5 +19,33 @@ namespace eCommerceWebApp.Controllers
 
             return View(categoryList);
         }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateCategory(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                Category category = new Category
+                {
+                    CategoryName = model.CategoryName,
+                    DisplayOrder = model.DisplayOrder
+                };
+
+                _dbContext.Categories.Add(category);
+
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
     }
 }
--- eCommerceWebApp/Models/Category.cs
+++ eCommerceWebApp/Models/Category.cs
@@ -8,10 +8,14 @@ namespace eCommerceWebApp.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name ="Category Name")]
         public string CategoryName { get; set; }
 
+        [Display(Name = "Display Order")]
+        [Range(1,999, ErrorMessage ="Display Order must be between 1 and 999")]
         public int DisplayOrder { get; set; }
 
+        [Display(Name = "Created Date")]
         public DateTime CategoryCreatedDateTime { get; set; } = DateTime.Now;
 
     }
