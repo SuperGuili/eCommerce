d43dc2de4597d5f49de23bc2a52f6872a5b402a8
luiz guilherme luize pereira <superguili@gmail.com>
2022-01-14 11:41:59 +0000

    Edit and delete category, Toastr notifications

--- eCommerceWebApp/Controllers/CategoryController.cs
+++ eCommerceWebApp/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace eCommerceWebApp.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Category> categoryList = _dbContext.Categories;
+            IEnumerable<Category> categoryList = _dbContext.Categories.OrderBy(c => c.DisplayOrder);
 
             return View(categoryList);
         }
@@ -42,10 +42,90 @@ namespace eCommerceWebApp.Controllers
 
                 _dbContext.SaveChanges();
 
+                TempData["success"] = "Category Created Succesfully";
+
                 return RedirectToAction("Index");
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult EditCategory(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            Category category = _dbContext.Categories.Find(Id);
+            //Category category = _dbContext.Categories.FirstOrDefault(c => c.Id==Id);
+            //Category category = _dbContext.Categories.SingleOrDefault(c => c.Id==Id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditCategory(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(model);
+
+                _dbContext.SaveChanges();
+
+                TempData["success"] = "Category Updated Succesfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult DeleteCategory(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            Category category = _dbContext.Categories.Find(Id);
+            //Category category = _dbContext.Categories.FirstOrDefault(c => c.Id==Id);
+            //Category category = _dbContext.Categories.SingleOrDefault(c => c.Id==Id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+
+        }
+
+        [HttpPost, ActionName("DeleteCategory")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteCategory(Category model)
+        {
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Categories.Remove(model);
+
+            _dbContext.SaveChanges();
+
+            TempData["success"] = "Category Deleted Succesfully";
+
+            return RedirectToAction("Index");
+
         }
     }
 }
