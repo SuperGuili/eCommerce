1bb2aa56bf788ec542502d0d3bad74350fa2191c
luiz guilherme luize pereira <superguili@gmail.com>
2022-01-21 14:18:32 +0000

    Home page and Details View

---
+++ eCommerce.Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class ShoppingCart
+    {
+        public Product Product { get; set; }
+
+        [Range(1,99, ErrorMessage ="Please enter a quantity between 1 and 99.")]
+        public int Count { get; set; }
+    }
+}
--- eCommerceWebApp/Areas/Customer/Controllers/HomeController.cs
+++ eCommerceWebApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using eCommerce.Models;
+﻿using eCommerce.DataAccess.Repository.IRepository;
+using eCommerce.Models;
+using eCommerce.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +10,30 @@ namespace eCommerceWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,Tag");    
+
+            return View(productList);
+        }
+
+        public IActionResult DetailsProduct(int id)
+        {
+            ShoppingCart shoppingCart = new()
+            {
+                Product = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == id, includeProperties: "Category,Tag"),
+                Count = 1
+            };
+
+            return View(shoppingCart);
         }
 
         public IActionResult Privacy()
