7dba41b60ae45171517bf31ec394d51d36fd16c4
Luiz Guilherme Pereira <superguili@gmail.com>
2022-02-04 14:04:29 +0000

    Implemented Create Order

--- eCommerce.Utility/StaticDetails.cs
+++ eCommerce.Utility/StaticDetails.cs
@@ -13,6 +13,18 @@ namespace eCommerce.Utility
 
         public const string Role_User_Indiv = "Individual";
         public const string Role_User_Company = "Company";
-        
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusRejected = "Rejected";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+
     }
 }
--- eCommerceWebApp/Areas/Customer/Controllers/CartController.cs
+++ eCommerceWebApp/Areas/Customer/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using eCommerce.DataAccess.Repository.IRepository;
 using eCommerce.Models;
 using eCommerce.Models.ViewModels;
+using eCommerce.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -12,6 +13,8 @@ namespace eCommerceWebApp.Areas.Customer.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
+        [BindProperty] // Binds this property when posting the form, no need for parameters 
         public ShoppingCartVM ShoppingCartVM { get; set; }
 
 
@@ -77,6 +80,57 @@ namespace eCommerceWebApp.Areas.Customer.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[ActionName("Summary")]
+        public IActionResult Summary(ShoppingCartVM ShoppingCartVM)
+        {
+            // Procedure to get the user ID
+            var clainsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = clainsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userID = claim.Value;
+
+            //Load the cart List from database
+            ShoppingCartVM.ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userID, includeProperties: "Product");
+
+            //Modify some orderHeader details
+            ShoppingCartVM.OrderHeader.PaymentStatus = StaticDetails.PaymentStatusPending;
+            ShoppingCartVM.OrderHeader.OrderStatus = StaticDetails.StatusPending;
+            ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+            ShoppingCartVM.OrderHeader.ApplicationUserId = userID;
+
+            //Calculate the Order total
+            foreach (var cart in ShoppingCartVM.ListCart)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price10, cart.Product.Price20);
+                ShoppingCartVM.OrderHeader.OrderTotal += (cart.Count * cart.Price);
+            }
+
+            _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+
+            //Get all the Order details
+            foreach (var cart in ShoppingCartVM.ListCart)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    OrderId = ShoppingCartVM.OrderHeader.Id,
+                    ProductId = cart.ProductId,
+                    Price = cart.Price,
+                    Count = cart.Count                    
+                };
+
+                _unitOfWork.OrderDetail.Add(orderDetail);
+                _unitOfWork.Save();
+            }
+
+            //Remove the items from the ShoppingCart database
+            _unitOfWork.ShoppingCart.RemoveRange(ShoppingCartVM.ListCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Index", "Home");
+
+        }
 
         public IActionResult Plus(int cartId)
         {
